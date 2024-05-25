98d1743c1ec672f2eeaee97105793b68e3b2ee1b
jeff muriuki <jeff.muriuki@studet.moringaschool.com>
2024-05-25 16:08:40 +0300

    fileio

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,26 @@
-def write_file(file_name, file_content):
-    pass
+from pathlib import Path
 
-def append_file(file_name, append_content):
-    pass
+def write_file(file_name, file_content):
+    file_name = Path(file_name)
+    if not file_name.suffix == '.txt':
+        file_name = file_name.with_suffix('.txt')
+    with open(file_name, mode='w', encoding='utf-8') as file:
+        file.write(file_content)   
+
+def append_file(file_name, file_content):
+    file_name = Path(file_name)
+    if not file_name.suffix == '.txt':
+        file_name = file_name.with_suffix('.txt')
+    with open(file_name, mode='a', encoding='utf-8') as file:
+        file.write(file_content)
 
 def read_file(file_name):
-    pass
+    file_name = Path(file_name)
+    if not file_name.suffix == '.txt':
+        file_name = file_name.with_suffix('.txt')
+    
+    if file_name.exists():
+        with open(file_name, 'r', encoding='utf-8') as file:
+            return file.read()
+    else:
+        return "File not found."
